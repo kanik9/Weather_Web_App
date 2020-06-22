d0b983bc82da7db491bf8dfbd84779c49da3b9f5
Kanik vijay <kanikvijay9@gmail.com>
2020-06-22 21:44:03 +0530

    Complete File

---
+++ weather/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import index , delete_city
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('delete/<city_name>/', delete_city, name='delete_city')
+]
